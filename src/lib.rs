3a70cc01f39ab902f2855377f4d0446f56db1415
Kaare Rasmussen <kaare@jasonic.dk>
2017-06-16 17:54:12 +0200

    Add tests for date type

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,14 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+extern crate libc;
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
+    use libc::c_char;
+    use std::ffi::CString;
 
     #[test]
     fn pg_numeric() {
@@ -19,6 +24,32 @@ mod tests {
             res = PGTYPESnumeric_to_int(&mut num1, &mut int1);
             println!("res: {}", res);
             println!("int: {}", int1);
+        }
+    }
+
+    #[test]
+    fn pg_date() {
+        unsafe {
+            let dt_str = CString::new("1959-4-11").unwrap().into_raw();
+            let mut tmp: *mut c_char = 0 as *mut c_char;
+            let dt1: date = PGTYPESdate_from_asc(dt_str, &mut tmp);
+            let mut dow = PGTYPESdate_dayofweek(dt1);
+            println!("dow: {}", dow);
+
+            // let mut iv1: interval = *PGTYPESinterval_new();
+            let mut ts1: timestamp = PGTYPEStimestamp_from_asc(dt_str, &mut tmp);
+            let iv_str = CString::new("58 years 3 weeks").unwrap().into_raw();
+            let mut iv1: interval = *PGTYPESinterval_from_asc(iv_str, &mut tmp);
+            let res = PGTYPEStimestamp_add_interval(&mut ts1, &mut iv1, &mut ts1);
+            println!("add res: {}", res);
+            let dt2 = PGTYPESdate_from_timestamp(ts1);
+            dow = PGTYPESdate_dayofweek(dt2);
+            println!("dow: {}", dow);
+
+            // let a: std::os::raw::c_char = *PGTYPESdate_to_asc(dt2);
+            let a = CString::from_raw(PGTYPESdate_to_asc(dt2));
+            let b = a.into_string().unwrap();
+            println!("date: {}", b);
         }
     }
 }
