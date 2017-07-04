9e99fe3ac791cc93b6895865a233292a1f366129
Kaare Rasmussen <kaare@jasonic.dk>
2017-07-04 19:58:05 +0200

    Implement std::ops traits for numeric, f64

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![allow(non_camel_case_types)]
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 extern crate libc;
@@ -8,7 +10,7 @@ use std::ffi::CString;
 impl From<i32> for numeric {
     fn from (v: i32) -> numeric {
         let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
-        let res = unsafe {PGTYPESnumeric_from_int(v, &mut num1)};
+        unsafe {PGTYPESnumeric_from_int(v, &mut num1)};
         num1
     }
 }
@@ -16,7 +18,7 @@ impl From<i32> for numeric {
 impl From<f64> for numeric {
     fn from (v: f64) -> numeric {
         let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
-        let res = unsafe {PGTYPESnumeric_from_double(v, &mut num1)};
+        unsafe {PGTYPESnumeric_from_double(v, &mut num1)};
         num1
     }
 }
@@ -27,6 +29,58 @@ impl<'a>  From<&'a str> for numeric {
         let c = CString::new(v).unwrap().into_raw();
         let mut tmp: *mut i8 = 0 as *mut i8;
         let num1 = unsafe {*PGTYPESnumeric_from_asc(c, &mut tmp)};
+        num1
+    }
+}
+
+impl Add<numeric> for numeric {
+    type Output = Self;
+
+    fn add (self, mut v: numeric) -> Self {
+        let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num3 = self;
+        unsafe {
+            PGTYPESnumeric_add(&mut num3, &mut v, &mut num1);
+        };
+        num1
+    }
+}
+
+impl Sub<numeric> for numeric {
+    type Output = Self;
+
+    fn sub (self, mut v: numeric) -> Self {
+        let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num3 = self;
+        unsafe {
+            PGTYPESnumeric_sub(&mut num3, &mut v, &mut num1);
+        };
+        num1
+    }
+}
+
+impl Mul<numeric> for numeric {
+    type Output = Self;
+
+    fn mul (self, mut v: numeric) -> Self {
+        let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num3 = self;
+        unsafe {
+            PGTYPESnumeric_mul(&mut num3, &mut v, &mut num1);
+        };
+        num1
+    }
+}
+
+impl Div<numeric> for numeric {
+    type Output = Self;
+
+    fn div (self, mut v: numeric) -> Self {
+        let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num3 = self;
+        unsafe {
+            PGTYPESnumeric_div(&mut num3, &mut v, &mut num1);
+        };
         num1
     }
 }
@@ -85,6 +139,66 @@ impl Div<i32> for numeric {
         let mut num3 = self;
         unsafe {
             PGTYPESnumeric_from_int(v, &mut num2);
+            PGTYPESnumeric_div(&mut num3, &mut num2, &mut num1)
+        };
+        num1
+    }
+}
+
+impl Add<f64> for numeric {
+    type Output = Self;
+
+    fn add (self, v: f64) -> Self {
+        let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num2: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num3 = self;
+        unsafe {
+            PGTYPESnumeric_from_double(v, &mut num2);
+            PGTYPESnumeric_add(&mut num3, &mut num2, &mut num1);
+        };
+        num1
+    }
+}
+
+impl Sub<f64> for numeric {
+    type Output = Self;
+
+    fn sub (self, v: f64) -> Self {
+        let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num2: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num3 = self;
+        unsafe {
+            PGTYPESnumeric_from_double(v, &mut num2);
+            PGTYPESnumeric_sub(&mut num3, &mut num2, &mut num1);
+        };
+        num1
+    }
+}
+
+impl Mul<f64> for numeric {
+    type Output = Self;
+
+    fn mul (self, v: f64) -> Self {
+        let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num2: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num3 = self;
+        unsafe {
+            PGTYPESnumeric_from_double(v, &mut num2);
+            PGTYPESnumeric_mul(&mut num3, &mut num2, &mut num1)
+        };
+        num1
+    }
+}
+
+impl Div<f64> for numeric {
+    type Output = Self;
+
+    fn div (self, v: f64) -> Self {
+        let mut num1: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num2: numeric = unsafe { *PGTYPESnumeric_new() };
+        let mut num3 = self;
+        unsafe {
+            PGTYPESnumeric_from_double(v, &mut num2);
             PGTYPESnumeric_div(&mut num3, &mut num2, &mut num1)
         };
         num1
